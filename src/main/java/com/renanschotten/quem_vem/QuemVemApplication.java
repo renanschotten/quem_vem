46e7bb17575473100aca5340d03b3b6481423b25
Renan Schotten <renan.schotten@gmail.com>
2023-08-25 10:20:05 -0300

    first commit

---
+++ src/main/java/com/renanschotten/quem_vem/QuemVemApplication.java
@@ -0,0 +1,13 @@
+package com.renanschotten.quem_vem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuemVemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuemVemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renanschotten/quem_vem/QuemVemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renanschotten.quem_vem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuemVemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
